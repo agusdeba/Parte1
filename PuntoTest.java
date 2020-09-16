186521e83321e0bad591a3902ab48c1d7727ee4a
agusdeba <agusdebattista@gmail.com>
2020-09-16 10:37:47 -0300

    commit resolviendo clone

--- PuntoTest.java
+++ PuntoTest.java
@@ -3,6 +3,6 @@ package Proyecto1_TDP;
 public class PuntoTest {
 	public static void main(String [] args) {
 		Punto p1 = new Punto(2,4);
-		System.out.println("Punto: "+p1.toString());
+		System.out.println("Este es el punto: "+p1.toString());
 	}
 }
