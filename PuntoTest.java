7e8bb802c28856d7334dc567377fcc0cdd3e540e
agusdeba <agusdebattista@gmail.com>
2020-09-16 10:46:13 -0300

    Resolucion de conflicto

--- PuntoTest.java
+++ PuntoTest.java
@@ -3,6 +3,6 @@ package Proyecto1_TDP;
 public class PuntoTest {
 	public static void main(String [] args) {
 		Punto p1 = new Punto(2,4);
-		System.out.println("Punto: "+p1.toString());
+		System.out.println("Hola, como va? Este es el punto: "+p1.toString());
 	}
 }
