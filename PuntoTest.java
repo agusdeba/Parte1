af11d3c2e9b04ef2f7e5e27c63c2b5b14468fbb4
agusdeba <agusdebattista@gmail.com>
2020-09-16 11:51:45 -0300

    Resolucion entre el repositorio remoto y local

--- PuntoTest.java
+++
@@ -1,9 +0,0 @@
-package Proyecto1_TDP;
-
-public class PuntoTest {
-	public static void main(String [] args) {
-		Punto p1 = new Punto(2,4);
-		System.out.println("Hola, como va? Este es el punto: "+p1.toString());
-		System.out.println("Este es el punto: "+p1.toString());
-	}
-}
