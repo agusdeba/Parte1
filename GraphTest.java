7d95e19a35494da3ca4d9e749f9eb143652f2680
agusdeba <71238269+agusdeba@users.noreply.github.com>
2020-09-27 17:57:54 -0300

    Add files via upload

---
+++ GraphTest.java
@@ -0,0 +1,27 @@
+package Parte_2;
+
+public class GraphTest {
+	public static void main(String [] args) {
+		Graph grafo = new Graph();
+		
+		grafo.addNode(29);
+        grafo.addNode(32);
+        grafo.addNode(56);
+        grafo.addNode(98);
+        grafo.addNode(45);
+
+        grafo.addEdge(29, 45);
+        grafo.addEdge(45, 29);
+        grafo.addEdge(45, 3);
+        
+        grafo.removeNode(29);
+        grafo.removeNode(29);
+
+        grafo.addEdge(29, 45);
+
+        
+		
+	
+	
+	}
+}
